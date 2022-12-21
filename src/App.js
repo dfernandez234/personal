0a26d300698771a2713e1f8e31159f13c7af1122
David Fernandez <62408734+dfernandez234@users.noreply.github.com>
2022-12-21 12:14:23 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,8 +17,11 @@ function App() {
       <Projects />
       <Container style={{'paddingTop':'3rem'}}>
         <h2>Get in Contact</h2>
-        <p>I Believe we can achieve great thhings togeether. Please send me a message in LinkedIn and add me on GitHub!</p>
+        <p>I Believe we can achieve great things together. Please send me a message in LinkedIn and add me on GitHub!</p>
         <p>I'm passionate about technology and always looking forward to improve the life of everyone around me, even if it is just one bit.</p>
+        <br/>
+        <h2>About me</h2>
+        <p>Kind of weird how I ended up in Software. I'm originally a Telecommunications Engineer who still has no idea how a radio works, I went into that major hoping I could work configuring routers all day since I didn't like to code. Been coding since I'm 13 went from Pascal to TurboC to C, maybe that's why I went into Telecommunications. But one day I realized what I love to do is creating things, and what better way than this.</p>
         <AnimatedCity/>
       </Container>
       <Footer />
