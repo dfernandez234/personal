7c7de1e02c862620ac518f0bd86af8606941e520
dfernandez234 <62408734+dfernandez234@users.noreply.github.com>
2022-12-20 23:58:17 -0600

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -57,7 +57,7 @@ export const Banner = () => {
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to my Portfolio</span>
                 <h1>{`Hi! I'm David`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>My name is David, I'm you average C++ enojoyer. Currently working as a Software Engineer maining C3 and ASP.NET as my prefered tools.</p>
+                  <p>My name is David, I'm you average C++ enojoyer. Currently working as a Software Engineer maining C# and ASP.NET as my tools.</p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
             </TrackVisibility>
